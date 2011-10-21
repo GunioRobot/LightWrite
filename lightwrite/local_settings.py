0688b912c9127b93a8030a27187c76b0a076b890
Gun.io Whitespace Robot <contact@gun.io>
2011-10-21 08:30:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- lightwrite/local_settings.py
+++ lightwrite/local_settings.py
@@ -1,6 +1,6 @@
 MEDIA_ROOT = '/home/tuttle/Projects/LightWrite/static'
 SITE_ROOT = '/home/tuttle/Projects/LightWrite/'
 STATICFILES_DIRS = (
-  '/home/tuttle/Projects/LightWrite/static/', 
+  '/home/tuttle/Projects/LightWrite/static/',
   )
 
--- lightwrite/texts/views.py
+++ lightwrite/texts/views.py
@@ -10,14 +10,14 @@ from lightwrite.texts.models import Text, TextForm
 
 import json
 import random
-import string 
+import string
 
 # Redirect to a random page
 # If deploying, make sure you have a REDIR in your local_settings
 # TODO: Don't redirect to already taken pads
 def root(request):
     wash = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(9))
-    return HttpResponseRedirect(settings.REDIR + '/' + wash) 
+    return HttpResponseRedirect(settings.REDIR + '/' + wash)
 
 # The main event
 def write(request, wash):
@@ -26,12 +26,12 @@ def write(request, wash):
         if (len(t)==0):
             t = Text(wash=wash)
             t.text = request.POST['writearea']
-            t.save() 
+            t.save()
             t = Text.objects.filter(wash=wash)
         else:
-            t = t[0] 
+            t = t[0]
             t.text = request.POST['writearea']
-            t.save() 
+            t.save()
             t = Text.objects.filter(wash=wash)
         if 'save' in request.POST:
             return render_to_response('light.html', {'wash': wash,}, context_instance=RequestContext(request))
@@ -49,7 +49,7 @@ def json_get_text(request, wash):
         t = Text(wash=wash)
         # Display mac command as HTML entity
         t.text = '\n\nWelcome to LightWrite!\nby Gun.io\n\nLightWrite is a web-based clone of WriteRoom - it\'s a place to write your thoughts without any distractions.\n\nTo use LightWrite, just start typing! You can move your mouse over the bottom of the page to save your text online or export to a file. \n\nTo enter fullscreen mode, just press F11, or &#8984;F if you\'re using OSX.\n\nEnjoy!\n\nLove,\nTeam Gun.io'
-        t.save() 
+        t.save()
         t = Text.objects.filter(wash=wash)
     data = serializers.serialize("json", t)
     return HttpResponse(data, mimetype='application/json')
